c0b932c1f46c8c562df8ee7993c74ec73e8712d8
Lowell Wooden <lowell.wooden@gmail.com>
2021-07-04 22:23:33 -0400

    DTO for Item entity

---
+++ DTOs/ItemDTO.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Catalog.DTOs
+{
+     public record Item {
+
+        public Guid Id { get; init;}
+        public String Name {get; init;}
+        public decimal Price {get; init;}
+        public DateTimeOffset CreatedDate {get; init;}
+
+        
+    }
+}
