1e628129ee7c13df89030d3ab0634dc145edecb4
Lowell Wooden <lowell.wooden@gmail.com>
2021-07-04 22:22:52 -0400

    in memory repository

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -4,29 +4,31 @@ using System.Linq;
 using Catalog.Entities;
 
 namespace Catalog.Repositories {
-    
-        public class InMemItemsRepository {
+  
 
-            private readonly List<Item> items = new()
-            {
-                new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow},
-                new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow},
-                new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 30, CreatedDate = DateTimeOffset.UtcNow}      
-            };
+    public class InMemItemsRepository : IItemsRepository
+    {
 
-
-            public IEnumerable<Item> GetItems() 
-            {
-                return items;
-            }
+        private readonly List<Item> items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 30, CreatedDate = DateTimeOffset.UtcNow }
+        };
 
 
-            public Item GetItem(Guid id) 
-            {
-                return items.Where(item => item.Id == id).SingleOrDefault();
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
 
-            }
 
-        
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+
+        }
+
+
     }
 }
